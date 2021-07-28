d0b5f237b05102f301cb0a6972971722420532c7
Junbai Chen <2604427c@student.gla.ac.uk>
2021-07-28 23:38:34 +0800

    test_chapter4 completed

--- rango/views.py
+++ rango/views.py
@@ -6,5 +6,5 @@ def index(request):
     return render(request,'rango/index.html',context_dict)
 def about(request):
     #return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a>")
-    context_dict = {'boldmessage': 'This tutorial has been put together by Junbai Chen.'}
-    return render(request,'rango/about.html',context_dict)
+    #context_dict = {'boldmessage': 'This tutorial has been put together by Junbai Chen.'}
+    return render(request,'rango/about.html')
